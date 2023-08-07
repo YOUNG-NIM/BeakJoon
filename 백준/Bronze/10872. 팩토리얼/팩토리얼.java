6fe52674c8f0fa3919ea28289244ceec291b39d7
YOUNG <65683124+YOUNG-NIM@users.noreply.github.com>
2023-08-07 13:44:02 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int N = Integer.parseInt(br.readLine());
+        int sum = 1;
+
+        if(N != 0){
+            for(int i=1; i<=N; i++){
+                sum *= i;
+            }
+        }
+
+        System.out.println(sum);
+        
+    }
+}
