17a830f93dc00ee0019fcfae5e55836002f9e713
YOUNG <65683124+YOUNG-NIM@users.noreply.github.com>
2023-09-14 08:43:43 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 296 ms, Memory: 15964 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i=1; i<=N; i++){
+            dq.addLast(i);
+        }
+
+        sb.append("<");
+
+        while(dq.size() != 1){
+            for(int i=0; i<(K-1); i++){
+                dq.addLast(dq.pollFirst());
+            }
+            sb.append(dq.pollFirst() + ", ");
+        }
+
+        sb.append(dq.pollFirst() + ">");
+
+        System.out.println(sb.toString().trim());
+
+    }
+
+}
